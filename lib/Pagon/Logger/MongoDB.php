09313c08ab1a4b05e89f8f1a1b1150e0a63c28ab
hfcorriez <hfcorriez@gmail.com>
2013-08-27 00:04:11 +0800

    Fix MongoDB options

--- lib/Pagon/Logger/MongoDB.php
+++ lib/Pagon/Logger/MongoDB.php
@@ -10,7 +10,7 @@ class MongoDB extends LoggerInterface
         'database'   => 'logs',
         'collection' => 'logs',
         'client'     => null,
-        'injectors'  => array()
+        'options'    => array()
     );
 
     /**
@@ -31,13 +31,19 @@ class MongoDB extends LoggerInterface
         parent::__construct($injectors);
 
         if (!$this->injectors['client']) {
-            $this->injectors['client'] = new \MongoClient($this->injectors['dsn'], $this->injectors['injectors']);
+            $this->injectors['client'] = new \MongoClient($this->injectors['dsn'], $this->injectors['options']);
         }
 
+        // Set client and collection
         $this->client = $this->injectors['client'];
         $this->collection = $this->client->selectCollection($this->injectors['database'], $this->injectors['collection']);
     }
 
+    /**
+     * Write messages
+     *
+     * @return mixed|void
+     */
     public function write()
     {
         foreach ($this->messages as $message) {
